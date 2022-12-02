151c47f9a4936b0924ba3374cf4533867d22cf6e
Dmytro Osaulenko <osauldmy@gmail.com>
2022-12-02 12:45:51 +0100

    Day 02: some more refactoring

--- day_02.py
+++ day_02.py
@@ -2,12 +2,8 @@
 import pathlib
 
 
-with open(pathlib.Path(__file__).parent / "day_02.input.txt") as f:
-    data = f.read()
-
-
-def first(play: str) -> int:
-    opponent, you = map(int, play.split(" "))
+def first(round: str) -> int:
+    opponent, you = map(int, round.split(" "))
     # (1 - 2), (3 - 1), (2 - 3) each mod 3 result in 2 -> win
     # (2 - 1), (1 - 3), (3 - 2) each mod 3 result in 1 -> lose
     # (1 - 1), (2 - 2), (3 - 3) each mod 3 result in 0 -> tie
@@ -15,8 +11,8 @@ def first(play: str) -> int:
     return you + (3 if score == 0 else 6 if score == 2 else 0)
 
 
-def second(play: str) -> int:
-    opponent, outcome = map(int, play.split(" "))
+def second(round: str) -> int:
+    opponent, outcome = map(int, round.split(" "))
     if outcome == 1:  # lose
         return 0 + (opponent - 1 if opponent in (2, 3) else 3)
     if outcome == 2:  # tie
@@ -25,10 +21,12 @@ def second(play: str) -> int:
     return 6 + (opponent + 1 if opponent in (1, 2) else 1)
 
 
-plays = data.translate(data.maketrans("AXBYCZ", "112233")).strip("\n").split("\n")
+translation = str.maketrans("AXBYCZ", "112233")
+with open(pathlib.Path(__file__).parent / "day_02.input.txt") as f:
+    rounds = f.read().translate(translation).strip("\n").split("\n")
 
 # First puzzle answer
-print(sum(map(first, plays)))
+print(sum(map(first, rounds)))
 
 # Second puzzle answer
-print(sum(map(second, plays)))
+print(sum(map(second, rounds)))
