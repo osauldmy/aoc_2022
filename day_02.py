b33b331fcfd73219bc7b6562deed9d811db78eaf
Dmytro Osaulenko <osauldmy@gmail.com>
2022-12-02 12:18:32 +0100

    Day 02: little refactoring

--- day_02.py
+++ day_02.py
@@ -1,6 +1,4 @@
 #!/bin/env python3
-from __future__ import annotations
-
 import pathlib
 
 
@@ -8,8 +6,8 @@ with open(pathlib.Path(__file__).parent / "day_02.input.txt") as f:
     data = f.read()
 
 
-def first(play: list[str]) -> int:
-    opponent, you = map(int, play)
+def first(play: str) -> int:
+    opponent, you = map(int, play.split(" "))
     # (1 - 2), (3 - 1), (2 - 3) each mod 3 result in 2 -> win
     # (2 - 1), (1 - 3), (3 - 2) each mod 3 result in 1 -> lose
     # (1 - 1), (2 - 2), (3 - 3) each mod 3 result in 0 -> tie
@@ -17,8 +15,8 @@ def first(play: list[str]) -> int:
     return you + (3 if score == 0 else 6 if score == 2 else 0)
 
 
-def second(play: list[str]) -> int:
-    opponent, outcome = map(int, play)
+def second(play: str) -> int:
+    opponent, outcome = map(int, play.split(" "))
     if outcome == 1:  # lose
         return 0 + (opponent - 1 if opponent in (2, 3) else 3)
     if outcome == 2:  # tie
@@ -27,12 +25,7 @@ def second(play: list[str]) -> int:
     return 6 + (opponent + 1 if opponent in (1, 2) else 1)
 
 
-plays = tuple(
-    map(
-        str.split,
-        data.translate(data.maketrans("AXBYCZ", "112233")).strip("\n").split("\n"),
-    )
-)
+plays = data.translate(data.maketrans("AXBYCZ", "112233")).strip("\n").split("\n")
 
 # First puzzle answer
 print(sum(map(first, plays)))
