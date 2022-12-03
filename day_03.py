2d42cfb8b5aff0707cbf62565bdf8e5317ad4c1d
Dmytro Osaulenko <osauldmy@gmail.com>
2022-12-03 17:53:11 +0100

    Add day 03

---
+++ day_03.py
@@ -0,0 +1,28 @@
+#!/bin/env python3
+import functools
+import pathlib
+import string
+
+import more_itertools
+
+
+with open(pathlib.Path(__file__).parent / "day_03.input.txt") as f:
+    rucksacks = f.read().strip("\n").split("\n")
+
+priorities = dict(zip(string.ascii_letters, range(1, len(string.ascii_letters) + 1)))
+
+halfstrings_intersects = map(
+    set.pop, map(lambda s: set(s[len(s) // 2 :]) & set(s[: len(s) // 2]), rucksacks)
+)
+
+# First puzzle answer
+print(sum(map(priorities.get, halfstrings_intersects)))
+
+
+triplewise_intersects = map(
+    lambda x: functools.reduce(set.intersection, x).pop(),
+    more_itertools.chunked(map(set, rucksacks), 3),
+)
+
+# Second puzzle answer
+print(sum(map(priorities.get, triplewise_intersects)))
