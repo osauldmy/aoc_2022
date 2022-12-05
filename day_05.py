b2ec2d7d2a00f2f0935accbedec1d089220eb950
Dmytro Osaulenko <osauldmy@gmail.com>
2022-12-05 21:10:00 +0100

    Add day 05

---
+++ day_05.py
@@ -0,0 +1,39 @@
+#!/bin/env python3
+import collections
+import copy
+import operator
+import pathlib
+import re
+
+with open(pathlib.Path(__file__).parent / "day_05.input.txt") as f:
+    text_stacks, text_moves = f.read().strip("\n").split("\n\n")
+
+stacks_n = 9
+stack_col_index = lambda i: 1 + 4 * (i - 1)
+col = lambda i: tuple(row[i] for row in text_stacks.split("\n"))
+
+stacks = tuple(
+    map(
+        lambda i: collections.deque(filter(str.isalpha, col(i))),
+        map(stack_col_index, range(1, stacks_n + 1)),
+    )
+)
+
+pattern = r"move (\d+) from (\d+) to (\d+)"
+moves = tuple(map(lambda x: tuple(map(int, x)), re.findall(pattern, text_moves)))
+
+backup = copy.deepcopy(stacks)
+for (count, src, dest) in moves:
+    stacks[dest - 1].extendleft(stacks[src - 1].popleft() for _ in range(count))
+
+# First puzzle answer
+print("".join(map(operator.itemgetter(0), stacks)))
+
+stacks = backup
+for (count, src, dest) in moves:
+    stacks[dest - 1].extendleft(
+        reversed(tuple(stacks[src - 1].popleft() for _ in range(count)))
+    )
+
+# Second puzzle answer
+print("".join(map(operator.itemgetter(0), stacks)))
