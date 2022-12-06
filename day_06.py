6a6741a8f0ff9e186dad74fa60462247ca7190dd
Dmytro Osaulenko <osauldmy@gmail.com>
2022-12-06 16:51:30 +0100

    Day 06: DRY

--- day_06.py
+++ day_06.py
@@ -6,14 +6,16 @@ import more_itertools
 with open(pathlib.Path(__file__).parent / "day_06.input.txt") as f:
     data = f.read().strip("\n")
 
-for index, window in enumerate(more_itertools.sliding_window(data, 4)):
-    if len(set(window)) == 4:
-        # First puzzle answer
-        print(4 + index)
-        break
 
-for index, window in enumerate(more_itertools.sliding_window(data, 14)):
-    if len(set(window)) == 14:
-        # Second puzzle answer
-        print(14 + index)
-        break
+def find_index(sequence: str, n: int) -> int:
+    for index, window in enumerate(more_itertools.sliding_window(sequence, n)):
+        if len(set(window)) == n:
+            break
+    return n + index
+
+
+# First puzzle answer
+print(find_index(data, 4))
+
+# Second puzzle answer
+print(find_index(data, 14))
